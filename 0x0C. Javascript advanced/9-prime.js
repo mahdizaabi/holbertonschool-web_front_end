c29f1325df73cc544a497bd6b6f4f08778c0d540
mahdizaabi <mahdi.zaabi@gmail.com>
2021-02-16 13:21:59 +0100

    timing execution
    timing execution

---
+++ 0x0C. Javascript advanced/9-prime.js
@@ -0,0 +1,20 @@
+/* Prime numbers & timing execution */
+const countPrimeNumbers = () => {
+  let x = 0;
+  
+  for (let i = 2; i <= 100; i++) {
+    x = 0;
+    for (let k = 2; k <= i; k++) {
+      if (i % k === 0 && i != k) {
+        x = 1;
+      }
+    }
+    if (x == 0) {
+      console.log(i);
+    }
+  }
+  navObject = performance.now()
+  console.log(`Execution time of printing countPrimeNumbers was ${navObject} milliseconds.`)
+};
+
+countPrimeNumbers();
